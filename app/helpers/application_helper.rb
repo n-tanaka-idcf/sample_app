8a4216950533f06f1ac3f217ab5412731051eeba
Nobuchika Tanaka <n.tanaka@idcf.jp>
2024-09-13 04:11:16 +0900

    title表示の改善

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_tile = 'Ruby on Rails Tutorial Sample App'
+    if page_title.empty?
+      base_tile
+    else
+      "#{page_title} | #{base_tile}"
+    end
+  end
 end
